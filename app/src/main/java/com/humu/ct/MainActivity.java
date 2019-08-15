641b8b6081d14a1dafc96ed205157f67b313c6ec
Godikov <13776573268@163.com>
2019-08-15 15:57:58 +0800

    修复子线程倒计时无效的bug。

--- app/src/main/java/com/humu/ct/MainActivity.java
+++ app/src/main/java/com/humu/ct/MainActivity.java
@@ -25,9 +25,54 @@ public class MainActivity extends AppCompatActivity {
 
         tv_60 = (TextView) findViewById(R.id.tv_60);
         tv_30 = (TextView) findViewById(R.id.tv_30);
+
     }
 
     public void count60(View view) {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                CTimer.start(COUNT_60, 60, new CTimer.CTimerListener() {
+                    @Override
+                    public void onCountDownStart(int tag) {
+                        tv_60.setText("开始倒计时");
+                        Log.d(TAG, "====onCountDownStart====" + tag);
+                    }
+
+                    @Override
+                    public void onCount(int tag, int timeNow) {
+                        tv_60.setText(timeNow + "");
+                        Log.d(TAG, "====onCount====" + tag + " " + timeNow);
+                    }
+
+                    @Override
+                    public void onCountPause(int tag, int timeNow) {
+                        Log.d(TAG, "====onCountPause====" + tag + " " + timeNow);
+                    }
+
+                    @Override
+                    public void onCountRestart(int tag) {
+                        Log.d(TAG, "====restartCount====" + tag);
+                    }
+
+                    @Override
+                    public void onCountOver(int tag) {
+                        tv_60.setText("倒计时结束");
+                        Log.d(TAG, "====onCountOver====" + tag);
+                    }
+
+                    @Override
+                    public void onStop(int tag) {
+                        tv_60.setText("倒计时手动结束");
+                        Log.d(TAG, "====onCountOver====" + tag);
+                    }
+                });
+
+            }
+        }).start();
+
+/*
         CTimer.start(COUNT_60, 60, new CTimer.CTimerListener() {
             @Override
             public void onCountDownStart(int tag) {
@@ -63,6 +108,7 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG,"====onCountOver===="+tag);
             }
         });
+*/
     }
 
     public void pause60(View view) {
@@ -82,35 +128,35 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCountDownStart(int tag) {
                 tv_30.setText("开始倒计时");
-                Log.d(TAG,"====onCountDownStart===="+tag);
+                Log.d(TAG, "====onCountDownStart====" + tag);
             }
 
             @Override
             public void onCount(int tag, int timeNow) {
-                tv_30.setText(timeNow+"");
-                Log.d(TAG,"====onCount===="+tag+" "+timeNow);
+                tv_30.setText(timeNow + "");
+                Log.d(TAG, "====onCount====" + tag + " " + timeNow);
             }
 
             @Override
             public void onCountPause(int tag, int timeNow) {
-                Log.d(TAG,"====onCountPause===="+tag+" "+timeNow);
+                Log.d(TAG, "====onCountPause====" + tag + " " + timeNow);
             }
 
             @Override
             public void onCountRestart(int tag) {
-                Log.d(TAG,"====restartCount===="+tag);
+                Log.d(TAG, "====restartCount====" + tag);
             }
 
             @Override
             public void onCountOver(int tag) {
                 tv_30.setText("倒计时结束");
-                Log.d(TAG,"====onCountOver===="+tag);
+                Log.d(TAG, "====onCountOver====" + tag);
             }
 
             @Override
             public void onStop(int tag) {
                 tv_30.setText("倒计时手动结束");
-                Log.d(TAG,"====onCountOver===="+tag);
+                Log.d(TAG, "====onCountOver====" + tag);
             }
         });
     }
--- timer/src/main/java/com/tchcn/timer/CTimer.java
+++ timer/src/main/java/com/tchcn/timer/CTimer.java
@@ -1,6 +1,7 @@
 package com.tchcn.timer;
 
 import android.os.Handler;
+import android.os.Looper;
 import android.os.Message;
 
 import java.util.ArrayList;
@@ -10,43 +11,72 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
- *  倒计时工具类。
+ * 倒计时工具类。
  * Created by humu on 2019/5/15.
  */
 
 public class CTimer {
 
-    private static final Map<Integer,TimerHandler> handlerMap = new ConcurrentHashMap<>();
-    private static final Map<Integer,Integer> timerMap = new ConcurrentHashMap<>();
-    private static final Map<Integer,Integer> intervalMap = new ConcurrentHashMap<>();
-    private static final Map<Integer,CTimerListener> listenerMap = new ConcurrentHashMap<>();
+    private static final Map<Integer, TimerHandler> handlerMap = new ConcurrentHashMap<>();
+    private static final Map<Integer, Integer> timerMap = new ConcurrentHashMap<>();
+    private static final Map<Integer, Integer> intervalMap = new ConcurrentHashMap<>();
+    private static final Map<Integer, CTimerListener> listenerMap = new ConcurrentHashMap<>();
     private static final List<Integer> pauseList = Collections.synchronizedList(new ArrayList<Integer>()); //正在暂停的倒计时
 
-    private CTimer(){}
+    private static Handler mainHandler = new Handler(Looper.getMainLooper());
+
+    private CTimer() {
+    }
+
+    private static boolean isMainLooper() {
+        if (Looper.myLooper() == Looper.getMainLooper()) {
+            return true;
+        }
+        return false;
+    }
 
     /**
      * 开始倒计时，默认倒计时间隔为1s，即每一秒回调一次onCount
-     * @param tag 倒计时标记
-     * @param totalTime 倒计时总时长，单位s
+     *
+     * @param tag            倒计时标记
+     * @param totalTime      倒计时总时长，单位s
      * @param cTimerListener 倒计时监听
      */
-    public static void start(int tag,int totalTime,CTimerListener cTimerListener){
-        if(!timerMap.containsKey(tag)){
-            TimerHandler handler = TimerHandler.getInstance();
-            synchronized (handlerMap){
-                handlerMap.put(tag,handler);
+    public static void start(final int tag, int totalTime, final CTimerListener cTimerListener) {
+        if (!timerMap.containsKey(tag)) {
+            final TimerHandler handler = TimerHandler.getInstance();
+            synchronized (handlerMap) {
+                handlerMap.put(tag, handler);
             }
-            Message message = handler.obtainMessage(tag);
-            if(totalTime > 0){
-                if(cTimerListener != null){
-                    cTimerListener.onCountDownStart(tag);
+            final Message message = handler.obtainMessage(tag);
+            if (totalTime > 0) {
+                if (cTimerListener != null) {
+                    if (isMainLooper()) {
+                        cTimerListener.onCountDownStart(tag);
+                    } else {
+                        mainHandler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                cTimerListener.onCountDownStart(tag);
+                            }
+                        });
+                    }
                 }
-                handler.sendMessage(message);
-                synchronized (timerMap){
-                    timerMap.put(tag,totalTime);
+                if (isMainLooper()) {
+                    handler.sendMessage(message);
+                } else {
+                    mainHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            handler.sendMessage(message);
+                        }
+                    });
                 }
-                synchronized (listenerMap){
-                    listenerMap.put(tag,cTimerListener);
+                synchronized (timerMap) {
+                    timerMap.put(tag, totalTime);
+                }
+                synchronized (listenerMap) {
+                    listenerMap.put(tag, cTimerListener);
                 }
             }
         }
@@ -54,31 +84,50 @@ public class CTimer {
 
     /**
      * 开始倒计时
-     * @param tag 倒计时标记
-     * @param totalTime 倒计时总时长，单位s
-     * @param intervalTime 倒计时间隔时间，即几秒回调一次onCount，单位s
+     *
+     * @param tag            倒计时标记
+     * @param totalTime      倒计时总时长，单位s
+     * @param intervalTime   倒计时间隔时间，即几秒回调一次onCount，单位s
      * @param cTimerListener 倒计时监听
      */
-    public static void start(int tag,int totalTime,int intervalTime,CTimerListener cTimerListener){
-        if(!timerMap.containsKey(tag)){
-            TimerHandler handler = TimerHandler.getInstance();
-            synchronized (handlerMap){
-                handlerMap.put(tag,handler);
+    public static void start(final int tag, int totalTime, int intervalTime, final CTimerListener cTimerListener) {
+        if (!timerMap.containsKey(tag)) {
+            final TimerHandler handler = TimerHandler.getInstance();
+            synchronized (handlerMap) {
+                handlerMap.put(tag, handler);
             }
-            Message message = handler.obtainMessage(tag);
-            if(totalTime > 0){
-                if(cTimerListener != null){
-                    cTimerListener.onCountDownStart(tag);
+            final Message message = handler.obtainMessage(tag);
+            if (totalTime > 0) {
+                if (cTimerListener != null) {
+                    if (isMainLooper()) {
+                        cTimerListener.onCountDownStart(tag);
+                    } else {
+                        mainHandler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                cTimerListener.onCountDownStart(tag);
+                            }
+                        });
+                    }
                 }
-                handler.sendMessage(message);
-                synchronized (timerMap){
-                    timerMap.put(tag,totalTime);
+                if (isMainLooper()) {
+                    handler.sendMessage(message);
+                } else {
+                    mainHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            handler.sendMessage(message);
+                        }
+                    });
                 }
-                synchronized (intervalMap){
-                    intervalMap.put(tag,intervalTime);
+                synchronized (timerMap) {
+                    timerMap.put(tag, totalTime);
                 }
-                synchronized (listenerMap){
-                    listenerMap.put(tag,cTimerListener);
+                synchronized (intervalMap) {
+                    intervalMap.put(tag, intervalTime);
+                }
+                synchronized (listenerMap) {
+                    listenerMap.put(tag, cTimerListener);
                 }
             }
         }
@@ -86,36 +135,47 @@ public class CTimer {
 
     /**
      * 暂停倒计时
+     *
      * @param tag 倒计时标记
      */
-    public synchronized static void pause(int tag){
-        if(!timerMap.containsKey(tag)){
+    public synchronized static void pause(int tag) {
+        if (!timerMap.containsKey(tag)) {
             return;
         }
-        if(pauseList.contains(tag)){
+        if (pauseList.contains(tag)) {
             return;
         }
-        synchronized (pauseList){
+        synchronized (pauseList) {
             pauseList.add(tag);
         }
     }
 
     /**
      * 继续倒计时
+     *
      * @param tag 被暂停的倒计时标记
      */
-    public static void resume(int tag){
-        if(!timerMap.containsKey(tag)){
+    public static void resume(final int tag) {
+        if (!timerMap.containsKey(tag)) {
             return;
         }
-        if(!pauseList.contains(tag)){
+        if (!pauseList.contains(tag)) {
             return;
         }
         pauseList.remove((Integer) tag);
-        if(listenerMap.containsKey(tag)){
-            CTimerListener cTimerListener = listenerMap.get(tag);
-            if(cTimerListener != null){
-                cTimerListener.onCountRestart(tag);
+        if (listenerMap.containsKey(tag)) {
+            final CTimerListener cTimerListener = listenerMap.get(tag);
+            if (cTimerListener != null) {
+                if (isMainLooper()) {
+                    cTimerListener.onCountRestart(tag);
+                } else {
+                    mainHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            cTimerListener.onCountRestart(tag);
+                        }
+                    });
+                }
             }
         }
         TimerHandler handler = TimerHandler.getInstance();
@@ -126,18 +186,28 @@ public class CTimer {
     /**
      * 停止倒计时
      * 会回调CTimerListener的onStop方法,不会回调CTimerListener的onCountOver方法
+     *
      * @param tag 倒计时标记
      */
-    public static void stop(int tag){
-        if(handlerMap.containsKey(tag)){
-            if(listenerMap.containsKey(tag)){
-                CTimerListener cTimerListener = listenerMap.get(tag);
-                if(cTimerListener != null){
-                    cTimerListener.onStop(tag);
+    public static void stop(final int tag) {
+        if (handlerMap.containsKey(tag)) {
+            if (listenerMap.containsKey(tag)) {
+                final CTimerListener cTimerListener = listenerMap.get(tag);
+                if (cTimerListener != null) {
+                    if (isMainLooper()) {
+                        cTimerListener.onStop(tag);
+                    } else {
+                        mainHandler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                cTimerListener.onStop(tag);
+                            }
+                        });
+                    }
                 }
             }
             TimerHandler handler = handlerMap.get(tag);
-            if(handler != null && handler.hasMessages(tag)){
+            if (handler != null && handler.hasMessages(tag)) {
                 handler.removeMessages(tag);
             }
             removeTag(tag);
@@ -147,9 +217,9 @@ public class CTimer {
     /**
      * 停止所有倒计时
      */
-    public synchronized static void stopAll(){
-        if(handlerMap.size() > 0){
-            for(int tag: handlerMap.keySet()){
+    public synchronized static void stopAll() {
+        if (handlerMap.size() > 0) {
+            for (int tag : handlerMap.keySet()) {
                 stop(tag);
             }
         }
@@ -158,18 +228,18 @@ public class CTimer {
     /**
      * 倒计时处理
      */
-    private static class TimerHandler extends Handler{
+    private static class TimerHandler extends Handler {
 
         private volatile static TimerHandler timerHandler;
 
-        private TimerHandler(){
+        private TimerHandler() {
 
         }
 
-        public static TimerHandler getInstance(){
-            if(timerHandler == null){
-                synchronized (TimerHandler.class){
-                    if(timerHandler == null){
+        public static TimerHandler getInstance() {
+            if (timerHandler == null) {
+                synchronized (TimerHandler.class) {
+                    if (timerHandler == null) {
                         timerHandler = new TimerHandler();
                     }
                 }
@@ -180,31 +250,61 @@ public class CTimer {
         @Override
         public synchronized void handleMessage(Message msg) {
             super.handleMessage(msg);
-            int what = msg.what;
-            if(listenerMap.containsKey(what)){
-                CTimerListener cTimerListener = listenerMap.get(what);
-                if(cTimerListener != null){
-                    if(timerMap.containsKey(what)){
+            final int what = msg.what;
+            if (listenerMap.containsKey(what)) {
+                final CTimerListener cTimerListener = listenerMap.get(what);
+                if (cTimerListener != null) {
+                    if (timerMap.containsKey(what)) {
                         Integer time = timerMap.get(what);
-                        if(time <= 0){
+                        if (time <= 0) {
                             removeTag(what);
-                            cTimerListener.onCountOver(what);
-                        }else if(time > 0){
-                            if(!pauseList.contains(what)){
-                                cTimerListener.onCount(what,time);
+                            if (isMainLooper()) {
+                                cTimerListener.onCountOver(what);
+                            } else {
+                                mainHandler.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        cTimerListener.onCountOver(what);
+                                    }
+                                });
+                            }
+                        } else {
+                            if (!pauseList.contains(what)) {
+                                if (isMainLooper()) {
+                                    cTimerListener.onCount(what, time);
+                                } else {
+                                    final Integer finalTime = time;
+                                    mainHandler.post(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            cTimerListener.onCount(what, finalTime);
+                                        }
+                                    });
+                                }
                                 time--;
-                                timerMap.put(what,time);
+                                timerMap.put(what, time);
                                 Message message = obtainMessage(what);
                                 int intervalTime = 1;
-                                if(intervalMap.containsKey(what)){
+                                if (intervalMap.containsKey(what)) {
                                     intervalTime = intervalMap.get(what);
                                 }
-                                sendMessageDelayed(message,intervalTime*1000);
-                            }else{
-                                cTimerListener.onCount(what,time);
-                                cTimerListener.onCountPause(what,time);
+                                sendMessageDelayed(message, intervalTime * 1000);
+                            } else {
+                                if (isMainLooper()) {
+                                    cTimerListener.onCount(what, time);
+                                    cTimerListener.onCountPause(what, time);
+                                } else {
+                                    final Integer finalTime1 = time;
+                                    mainHandler.post(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            cTimerListener.onCount(what, finalTime1);
+                                            cTimerListener.onCountPause(what, finalTime1);
+                                        }
+                                    });
+                                }
                                 time--;
-                                timerMap.put(what,time);
+                                timerMap.put(what, time);
                             }
                         }
                     }
@@ -215,31 +315,32 @@ public class CTimer {
 
     /**
      * 倒计时结束，移除该倒计时
+     *
      * @param tag
      */
-    private static void removeTag(int tag){
-        if(timerMap.containsKey(tag)){
-            synchronized (timerMap){
+    private static void removeTag(int tag) {
+        if (timerMap.containsKey(tag)) {
+            synchronized (timerMap) {
                 timerMap.remove(tag);
             }
         }
-        if(intervalMap.containsKey(tag)){
-            synchronized (intervalMap){
+        if (intervalMap.containsKey(tag)) {
+            synchronized (intervalMap) {
                 intervalMap.remove(tag);
             }
         }
-        if(listenerMap.containsKey(tag)){
-            synchronized (listenerMap){
+        if (listenerMap.containsKey(tag)) {
+            synchronized (listenerMap) {
                 listenerMap.remove(tag);
             }
         }
-        if(handlerMap.containsKey(tag)){
-            synchronized (handlerMap){
+        if (handlerMap.containsKey(tag)) {
+            synchronized (handlerMap) {
                 handlerMap.remove(tag);
             }
         }
-        if(pauseList.contains(tag)){
-            synchronized (pauseList){
+        if (pauseList.contains(tag)) {
+            synchronized (pauseList) {
                 pauseList.remove((Integer) tag);
             }
         }
@@ -252,41 +353,51 @@ public class CTimer {
 
         /**
          * 开始倒计时
+         *
          * @param tag 倒计时标记
          */
-        public void onCountDownStart(int tag){}
+        public void onCountDownStart(int tag) {
+        }
 
         /**
          * 倒计时中
-         * @param tag 倒计时标记
+         *
+         * @param tag     倒计时标记
          * @param timeNow 当前时间(s)
          */
-        public abstract void onCount(int tag,int timeNow);
+        public abstract void onCount(int tag, int timeNow);
 
         /**
          * 倒计时暂停
-         * @param tag 倒计时标记
+         *
+         * @param tag     倒计时标记
          * @param timeNow 暂停时的时间(s)
          */
-        public void onCountPause(int tag,int timeNow){}
+        public void onCountPause(int tag, int timeNow) {
+        }
 
         /**
          * 暂停后重新开始倒计时前回调
+         *
          * @param tag 倒计时标记
          */
-        public void onCountRestart(int tag){}
+        public void onCountRestart(int tag) {
+        }
 
         /**
          * 倒计时结束
+         *
          * @param tag 倒计时标记
          */
         public abstract void onCountOver(int tag);
 
         /**
          * 手动结束倒计时后回调
+         *
          * @param tag 倒计时标记
          */
-        public void onStop(int tag){}
+        public void onStop(int tag) {
+        }
 
     }
 
